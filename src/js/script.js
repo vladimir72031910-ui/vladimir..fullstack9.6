a0f0f9a353038055faa08583b710e54b35db2147
vova <vladimir72031910@gmail.com>
2026-06-07 20:12:25 +0900

    feat: all project

---
+++ src/js/script.js
@@ -0,0 +1,64 @@
+const swiper = new Swiper('.brands', {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  loop: true,
+  pagination: {
+    el:'.swiper-pagination',
+    clickable: true,
+  },
+   breakpoints: {
+    770: {
+        enabled: false,
+    }
+   }
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const buttons = document.querySelectorAll('.show-hidden');
+
+    buttons.forEach(function (button) {
+
+        const arrow = button.querySelector('.expand');
+        const textSpan = button.querySelector('.hiddens');
+
+        button.addEventListener('click', function () {
+
+            const section = button.closest('section');
+
+            const hiddenBlock = section.querySelector('.hidden-brands');
+
+            const isHidden = hiddenBlock.classList.toggle('hidden');
+
+            if (isHidden) {
+
+                textSpan.textContent = 'Показать все';
+                arrow.style.transform = 'rotate(0deg)';
+
+            } else {
+
+                textSpan.textContent = 'Скрыть';
+                arrow.style.transform = 'rotate(180deg)';
+
+            }
+
+        });
+
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerButton = document.querySelector('.burgerbutton');
+    const sidebar = document.querySelector('#sidebar');
+    const crossButton = sidebar.querySelector('#cross');
+
+    burgerButton.addEventListener('click', function () {
+        sidebar.classList.add('active');
+    });
+
+    crossButton.addEventListener('click', function () {
+        sidebar.classList.remove('active');
+    });
+});
